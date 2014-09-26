5083cd78680b0324d4e376eeb5f8d15f5e1e3a3d
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-09-26 14:22:25 +0400

    refactor

--- calculate_motif_statistics.rb
+++ calculate_motif_statistics.rb
@@ -9,125 +9,95 @@ def count_each_element(more_5_motif)
         result[motif_name] = 1
     end
   end
-  return result
+  result
 end  
 
+def cpg_mutation?(sequence)
+  (sequence[26] == 'C' && sequence[30] == 'G') || (sequence[26] == 'G' && sequence[24] == 'C')
+end
+
+def tpc_mutation?(sequence)
+  (sequence[26] == 'C' && sequence[24] == 'T') || (sequence[26] == 'G' && sequence[30] == 'A')
+end
+
+def count_each_motif_mutations(all_mutations_filename, mutations_subset, fold_change_cutoff)
+  File.open(all_mutations_filename) do |f|
+    # "27610826_3 MAZ_f1  -7  direct  cggctgaGgaggaggag -7  direct  cggctgaCgaggaggag G/C 1.1218764110455249E-4 9.602413003842941E-4  0.11683275970285215"
+    mutated_sites = f.each_line.drop(1).select do |line|
+      line_splitted = line.split("\t")
+      name_snp = line_splitted[0].split("_")[0]
+      (line_splitted[-1].to_f < fold_change_cutoff) && mutations_subset.include?(name_snp)
+    end
+    mutated_motifs = mutated_sites.map{|el| el.split("\t")[1] }
+    count_each_element(mutated_motifs)
+  end
+end
+
+def output_counts_for_each_motif(output_filename, motif_names, motif_counts)
+  File.open(output_filename, 'w') do |fw|
+    motif_names.each do |motif_name|
+      fw.puts "#{motif_name}\t#{motif_counts[motif_name] || 0}"
+    end
+  end
+end
+
+def filename_w_prefix(filename, prefix)
+  File.join(File.dirname(filename), prefix + File.basename(filename))
+end
+
+def motif_statistics(filename_result, file_prefix, motif_names, input_file, mutations_subset, fold_change_cutoff)
+  output_counts_for_each_motif(filename_w_prefix(filename_result, file_prefix), motif_names, 
+                            count_each_motif_mutations(input_file, mutations_subset, fold_change_cutoff)) 
+end
 
 filename = ARGV[0]   # real_mutations.txt
 filename_result = ARGV[1] # results_real_mutations.txt --> cpg_promoter_results_real_mutations.txt, tpc_intronic_results_real_mutations.txt
 
-motif_names = File.readlines('/home/ilya/iogen/BioSchool-summer2014/SNP/motif_names.txt').map(&:strip)
+Dir.mkdir File.dirname(filename_result) unless Dir.exist?(File.dirname(filename_result))
+
+raise 'Specify input file with mutations and output filename for results(actually 4 files will be created)'  unless filename && filename_result
+
+motif_names = File.readlines('./motif_names.txt').map(&:strip)
+
+snp_splitted = File.readlines('./SNPs.txt').map{|el| el.split("\t")}
+
+cpg_names = Set.new( snp_splitted.select {|name, sequence| cpg_mutation?(sequence) }.map{|name, sequence| name } ) 
+tpc_names = Set.new( snp_splitted.select {|name, sequence| tpc_mutation?(sequence) }.map{|name, sequence| name } )
+
+$stderr.puts "CpG: #{cpg_names.size}\nTpC: #{tpc_names.size}"
+
+mut_types = File.readlines('./SUBSTITUTIONS_13Apr2012_snz_promoter_markup2.txt').drop(1).map{|el| data = el.split("\t"); [data[0], data[17]] };
+intronic_mutations = mut_types.select {|mut_name, mut_type| mut_type.split(',').map(&:strip).include? 'Intronic' }
+promoter_mutations = mut_types.select {|mut_name, mut_type| mut_type.split(',').map(&:strip).include? 'Promoter' }
+
+regulatory_mutations = mut_types.select {|mut_name, mut_type| types = mut_type.split(',').map(&:strip); types.include?('Promoter') || types.include?('Intronic') }
+
+# mut_types_intronic_promoter = mut_types.select {|mut_name, mut_type| mut_type == 'Intronic,Promoter' }
+
+intronic_mutation_names = Set.new(intronic_mutations.map{|mut_name, mut_type| mut_name} )
+promoter_mutation_names = Set.new(promoter_mutations.map{|mut_name, mut_type| mut_name} )
+regulatory_mutation_names = Set.new(regulatory_mutations.map{|mut_name, mut_type| mut_name} )
+
+cpg_intronic_names = cpg_names & intronic_mutation_names
+tpc_intronic_names = tpc_names & intronic_mutation_names
+cpg_promoter_names = cpg_names & promoter_mutation_names
+tpc_promoter_names = tpc_names & promoter_mutation_names
+cpg_regulatory_names = cpg_names & regulatory_mutation_names
+tpc_regulatory_names = tpc_names & regulatory_mutation_names
 
 
-snp = File.readlines('/home/ilya/iogen/BioSchool-summer2014/SNP/SNPs.txt'); # ['name seq', 'name seq',...]
-snp_splitted = snp.map{|el| el.split("\t")};  # [['name', 'seq'], ['name', 'seq'], ...]
-
-# [['name','seq'],['name','seq'],['name', 'seq']... ] ---(select)---> [['name','seq'],..]  ---(map)---> ['name',...]
-
-cpg_seq = snp_splitted.select do |name, sequence|
-  ((sequence[26] == 'C') && (sequence[30] == 'G')) || ((sequence[26] == 'G') && (sequence[24] == 'C'))
-end
-cpg_names = Set.new( cpg_seq.map{|name, sequence| name } ) 
-
-#   ((sequence[26] == 'C') && (sequence[24] == 'T')) || ((sequence[26] == 'G') && (sequence[30] == 'A')) 
-tpc_seq = snp_splitted.select do |name, sequence|
-  ((sequence[26] == 'C') && (sequence[24] == 'T')) || ((sequence[26] == 'G') && (sequence[30] == 'A'))
-end
-tpc_names = Set.new( tpc_seq.map{|name, sequence| name } )
-
-#p cpg_names.size
-#p tpc_names.size
-#p cpg_names
-
-mut = File.readlines('/home/ilya/iogen/BioSchool-summer2014/SNP/SUBSTITUTIONS_13Apr2012_snz_promoter_markup2.txt').drop(1);
-mut_splitted = mut.map{|el| el.split("\t")};
-#mut_types = mut_splitted[0], 
-mut_types_intronic = mut_splitted.select do |el|
-  el[17] == 'Intronic' || el[17] == 'Intronic,Promoter'  
-end
-mut_types_promoter = mut_splitted.select do |el|
-  el[17] == 'Promoter' || el[17] == 'Intronic,Promoter' 
-end;  
-# mut_types_intronic_promoter = mut_splitted.select do |el|
-#   el[17] == 'Intronic,Promoter'
-# end;  
-mut_types_name_intronic = Set.new(mut_types_intronic.map{|el| el[0]});
-mut_types_name_promoter = Set.new(mut_types_promoter.map{|el| el[0]});
-
-
-# $stderr.puts "CpG: #{cpg_names.size}\nTpC: #{tpc_names.size}"
-# exit
-
+$stderr.puts "CpG intronic: #{cpg_intronic_names.size}\nCpG promoter: #{cpg_promoter_names.size}\nTpC intronic: #{tpc_intronic_names.size}\nTpC promoter: #{tpc_promoter_names.size}"
+$stderr.puts "Regulatory: #{regulatory_mutation_names.size}"
+$stderr.puts "CpG regulatory: #{cpg_regulatory_names.size}\nTpC regulatory: #{tpc_regulatory_names.size}"
 
 fold_change_cutoff = 1.0 / 5.0
-File.open('cpg_intronic_' + filename_result, 'w') do |fw|
-  File.open(filename) do |f|
-    cpg_more_5 = f.each_line.drop(1).select do |line| # "27610826_3 MAZ_f1  -7  direct  cggctgaGgaggaggag -7  direct  cggctgaCgaggaggag G/C 1.1218764110455249E-4 9.602413003842941E-4  0.11683275970285215"
-      line_splitted = line.split("\t") # ["27610826_3", "MAZ_f1",  "-7",  "direct",  "cggctgaGgaggaggag", "-7",  "direct",  "cggctgaCgaggaggag", "G/C", "1.1218764110455249E-4", "9.602413003842941E-4",  "0.11683275970285215"]
-      name_snp = line_splitted[0].split("_")[0]
-      (line_splitted[-1].to_f < fold_change_cutoff) && (cpg_names.include? name_snp) && (mut_types_name_intronic.include? name_snp)
-    end
-    cpg_more_5_motif_intronic = cpg_more_5.map{|el| el.split("\t")[1]}
-    counts = count_each_element(cpg_more_5_motif_intronic)
-    # counts.each do |name,num|
-    #   fw.puts name + "\t" + num.to_s
-    # end
-    motif_names.each do |motif_name|
-      fw.puts "#{motif_name}\t#{counts[motif_name] || 0}"
-    end
-  end
-end
 
-File.open('cpg_promoter_' + filename_result, 'w') do |fw|
-  File.open(filename) do |f|
-    cpg_more_5_promoter = f.each_line.drop(1).select do |line| # "27610826 MAZ_f1  -7  direct  cggctgaGgaggaggag -7  direct  cggctgaCgaggaggag G/C 1.1218764110455249E-4 9.602413003842941E-4  0.11683275970285215"
-      line_splitted = line.split("\t") # ["27610826", "MAZ_f1",  "-7",  "direct",  "cggctgaGgaggaggag", "-7",  "direct",  "cggctgaCgaggaggag", "G/C", "1.1218764110455249E-4", "9.602413003842941E-4",  "0.11683275970285215"]
-      name_snp = line_splitted[0].split("_")[0]
-      (line_splitted[-1].to_f < fold_change_cutoff) && (cpg_names.include? name_snp) && (mut_types_name_promoter.include? name_snp)
-    end
-    cpg_more_5_motif_promoter = cpg_more_5_promoter.map{|el| el.split("\t")[1]}
-    counts = count_each_element(cpg_more_5_motif_promoter)
-    # counts.each do |name,num|
-    #   fw.puts name + "\t" + num.to_s
-    # end
-    motif_names.each do |motif_name|
-      fw.puts "#{motif_name}\t#{counts[motif_name] || 0}"
-    end
-  end 
-end
 
-File.open('tpc_intronic_' + filename_result, 'w') do |fw|
-  File.open(filename) do |f|
-    tpc_more_5 = f.each_line.drop(1).select do |line| # "27610826 MAZ_f1  -7  direct  cggctgaGgaggaggag -7  direct  cggctgaCgaggaggag G/C 1.1218764110455249E-4 9.602413003842941E-4  0.11683275970285215"
-      line_splitted = line.split("\t") # ["27610826", "MAZ_f1",  "-7",  "direct",  "cggctgaGgaggaggag", "-7",  "direct",  "cggctgaCgaggaggag", "G/C", "1.1218764110455249E-4", "9.602413003842941E-4",  "0.11683275970285215"]
-      name_snp = line_splitted[0].split("_")[0]
-      (line_splitted[-1].to_f < fold_change_cutoff) && (tpc_names.include? name_snp) && (mut_types_name_intronic.include? name_snp)
-    end
-    tpc_more_5_motif_intronic = tpc_more_5.map{|el| el.split("\t")[1]} 
-    counts = count_each_element(tpc_more_5_motif_intronic)
-    # counts.each do |name,num|
-    #   fw.puts name + "\t" + num.to_s
-    # end
-    motif_names.each do |motif_name|
-      fw.puts "#{motif_name}\t#{counts[motif_name] || 0}"
-    end
-  end
-end
+motif_statistics(filename_result, 'cpg_regulatory_', motif_names, filename, cpg_regulatory_names, fold_change_cutoff)
+motif_statistics(filename_result, 'tpc_regulatory_', motif_names, filename, tpc_regulatory_names, fold_change_cutoff)
 
-File.open('tpc_promoter_' + filename_result, 'w') do |fw|
-  File.open(filename) do |f|
-    tpc_more_5_promoter = f.each_line.drop(1).select do |line| # "27610826 MAZ_f1  -7  direct  cggctgaGgaggaggag -7  direct  cggctgaCgaggaggag G/C 1.1218764110455249E-4 9.602413003842941E-4  0.11683275970285215"
-      line_splitted = line.split("\t") # ["27610826", "MAZ_f1",  "-7",  "direct",  "cggctgaGgaggaggag", "-7",  "direct",  "cggctgaCgaggaggag", "G/C", "1.1218764110455249E-4", "9.602413003842941E-4",  "0.11683275970285215"]
-      name_snp = line_splitted[0].split("_")[0]
-      (line_splitted[-1].to_f < fold_change_cutoff) && (tpc_names.include? name_snp) && (mut_types_name_promoter.include? name_snp)
-    end
-    tpc_more_5_motif_promoter = tpc_more_5_promoter.map{|el| el.split("\t")[1]}
-    counts = count_each_element(tpc_more_5_motif_promoter) 
-    # counts.each do |name,num|
-    #   fw.puts name + "\t" + num.to_s
-    # end
-    motif_names.each do |motif_name|
-      fw.puts "#{motif_name}\t#{counts[motif_name] || 0}"
-    end
-  end 
-end
+motif_statistics(filename_result, 'cpg_intronic_', motif_names, filename, cpg_intronic_names, fold_change_cutoff)
+motif_statistics(filename_result, 'tpc_intronic_', motif_names, filename, tpc_intronic_names, fold_change_cutoff)
+
+motif_statistics(filename_result, 'cpg_promoter_', motif_names, filename, cpg_promoter_names, fold_change_cutoff)
+motif_statistics(filename_result, 'tpc_promoter_', motif_names, filename, tpc_promoter_names, fold_change_cutoff)
