15c0692169cc21789462e17fc6d3f7961d2c4600
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-10-08 15:22:44 +0400

    combine_files now can  combine files row-by-row instead of columns-by-column

--- combine_files.rb
+++ combine_files.rb
@@ -1,7 +1,7 @@
 require 'optparse'
 
 class FileColumnCombiner
-  attr_reader :mode, :files
+  attr_reader :mode
   def initialize(mode, filenames)
     @mode = mode
     @files = filenames.map{|filename| File.open(filename) }
@@ -9,6 +9,7 @@ class FileColumnCombiner
   end
 
   def each_line
+    return enum_for(:each_line)  unless block_given?
     loop do
       yield next_line
     end
@@ -41,19 +42,69 @@ class FileColumnCombiner
   end
 end
 
+class FileRowCombiner
+  attr_reader :mode
+  def initialize(mode, filenames)
+    @mode = mode
+    @filenames = filenames
+    @file_number = 0
+    raise "Header mode can be one of:\n :one, :zero, :all"  unless [:one, :zero, :all].include?(mode)
+  end
+
+  def yield_first_line_if_necessary(first_row)
+    case mode
+    when :one
+      yield first_row  if @file_number == 1 
+    when :zero
+    when :all
+      yield first_row
+    end
+  end
+  
+  def yield_from_stream(stream, &block)
+    yield_first_line_if_necessary(stream.readline, &block)
+    stream.each_line do |line|
+      yield line
+    end
+  end
+
+  protected :yield_from_stream, :yield_first_line_if_necessary
+
+  def each_line(&block)
+    return enum_for(:each_line)  unless block_given?
+    @filenames.each do |filename|
+      @file_number += 1
+      File.open(filename) do |f|
+        yield_from_stream(f, &block)
+      end
+    end
+  end
+end
+
+
 mode = :all
+rows_or_columns = :columns
 OptionParser.new do |opts|
   opts.on('--header [MODE]',  "Has header column (sic!); Mode is one/zero/all. If MODE not specifed, `one` is supposed.\n" +
                               "If option not specified, tool works like `all` is supplied") {|value|
     mode = value ? value.to_sym : :one
     raise "Header mode can be one of: 'one', 'zero', 'all'."  unless [:one, :zero, :all].include?(mode)
   }
+  opts.on('--rows', "Glue rows, not columns") {
+    rows_or_columns = :rows
+  }
 end.parse!(ARGV)
 
 files = ARGV
 raise "Specify at least one file"  unless files.size >= 1
 
-combiner = FileColumnCombiner.new(mode, files)
+case rows_or_columns
+when :columns
+  combiner = FileColumnCombiner.new(mode, files)
+when :rows
+  combiner = FileRowCombiner.new(mode, files)
+end
+
 combiner.each_line do |line|
   puts line
 end
