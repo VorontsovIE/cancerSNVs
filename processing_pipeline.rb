187d58001feb41484dcafebae0b9cff9958b945e
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-10-12 00:27:25 +0400

    unindent

--- processing_pipeline.rb
+++ processing_pipeline.rb
@@ -9,30 +9,30 @@ def load_motif_qualities(filename)
 end
 
 def combine_results(mutation_type, context_type)
-    IO.popen(["ruby",  "combine_files.rb",
-                      "results/disrupted/shuffle/#{mutation_type}_#{context_type}_disrupted_shuffle.txt",
-                      "results/disrupted/cancer/#{mutation_type}_#{context_type}_disrupted_cancer.txt",
-                      "results/disrupted/shuffle/#{mutation_type}_#{context_type}_total_shuffle.txt",
-                      "results/disrupted/cancer/#{mutation_type}_#{context_type}_total_cancer.txt",
-                      "--header", "one"]) do |counts_io|
-      with_temp_file("counts.txt") do |counts_fw|
-        counts_fw.puts("motif\tdisrupted shuffle\tdisrupted cancer\ttotal shuffle\ttotal cancer")
-        counts_fw.print(counts_io.read)
-        counts_fw.close
+  IO.popen(["ruby",  "combine_files.rb",
+                    "results/disrupted/shuffle/#{mutation_type}_#{context_type}_disrupted_shuffle.txt",
+                    "results/disrupted/cancer/#{mutation_type}_#{context_type}_disrupted_cancer.txt",
+                    "results/disrupted/shuffle/#{mutation_type}_#{context_type}_total_shuffle.txt",
+                    "results/disrupted/cancer/#{mutation_type}_#{context_type}_total_cancer.txt",
+                    "--header", "one"]) do |counts_io|
+    with_temp_file("counts.txt") do |counts_fw|
+      counts_fw.puts("motif\tdisrupted shuffle\tdisrupted cancer\ttotal shuffle\ttotal cancer")
+      counts_fw.print(counts_io.read)
+      counts_fw.close
 
-        IO.popen("ruby holms_significance.rb --with-header --with-header-column #{counts_fw.path}") do |significances_io|
-          with_temp_file("significances.txt") do |significances_fw|
-            significances_fw.puts("motif\tHolms corrected P-value")
-            significances_fw.print(significances_io.read)
-            significances_fw.close
+      IO.popen("ruby holms_significance.rb --with-header --with-header-column #{counts_fw.path}") do |significances_io|
+        with_temp_file("significances.txt") do |significances_fw|
+          significances_fw.puts("motif\tHolms corrected P-value")
+          significances_fw.print(significances_io.read)
+          significances_fw.close
 
-            IO.popen(["ruby", "combine_files.rb", counts_fw.path, significances_fw.path, "--header", "one"]) do |result_io|
-              File.write("results/#{mutation_type}_#{context_type}.csv", result_io.read)
-            end
+          IO.popen(["ruby", "combine_files.rb", counts_fw.path, significances_fw.path, "--header", "one"]) do |result_io|
+            File.write("results/#{mutation_type}_#{context_type}.csv", result_io.read)
           end
         end
       end
     end
+  end
 end
 
 genes_by_motif = File.readlines('source_data/hocomoco_genes.csv').drop(1)
