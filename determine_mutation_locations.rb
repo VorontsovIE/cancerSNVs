f5122fe60f71b9b224dd665db7e1da94cfbc49cf
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-10-19 16:19:53 +0400

    interval tree renamed, new class for interval tree (yet empty) extracted

--- determine_mutation_locations.rb
+++ determine_mutation_locations.rb
@@ -4,36 +4,6 @@ require 'genome_region'
 require 'mutation_features'
 require 'support'
 
-# class IntervalTree
-#   attr_reader :chromosomes
-#   def initialize(interval_object_pairs)
-#     @chromosomes = interval_object_pairs
-#       .group_by{|interval, object| "#{interval.chromosome},#{interval.strand}" }
-#       .map{|chr, interval_object_pairs_subset|
-#         # p interval_object_pairs_subset
-#         intervals = interval_object_pairs_subset
-#         .each_with_index
-#         .sort_by{|(interval, object), ind|
-#           interval.pos_end
-#         }
-#         [chr,intervals]
-#       }.to_h#.tap{|x| p x}
-#   end
-
-#   def find(chr, strand, pos)
-#     (interval, obj), ind = @chromosomes["#{chr},#{strand}"].bsearch{|(interval, obj), ind| interval.pos_end >= pos } # ?
-#     @chromosomes["#{chr},#{strand}"].drop(ind).take_while{|(interval, obj), ind| interval.pos_end}
-#     interval.include_position?(pos) ? [interval, obj] : nil
-#     # return []  unless ind
-#     # # select tss-es in upstream and in downstream (because promoter region can be assymetric)
-#     # tsses[ [ind - 1, 0].max, 2 ].map{|tss,ind| tss}
-#   end
-# end
-#
-# interval_tree = IntervalTree.new(interval_object_pairs.first(20))
-# p interval_tree.find('CHR_HSCHR6_MHC_QBL_CTG1', :+, 29887830)
-
-
 def context_type(name_snp, context_types)
   context_pair = context_types.detect{|context_type, context_type_nameset| context_type_nameset.include?(name_snp) }
   context_pair && context_pair.first
