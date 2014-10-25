86eeea72408c2bb6ac8d83d38bd41a0868d47bbe
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-10-25 17:47:44 +0400

    histogram now has inner conversion procedure

--- lib/histogram.rb
+++ lib/histogram.rb
@@ -2,23 +2,19 @@
 # [from; to)
 class Histogram
   attr_reader :from, :to, :step, :elements_total, :elements_total_in_range, :less_than, :greater_than
-  def range; @range ||= (from...to); end
-  def bins; range.step(step).map{|left| (left...[left + step, to].min) }; end
-  def each_bin(ignore_flanks: false)
-    return enum_for(:each_bin, ignore_flanks: ignore_flanks)  unless block_given?
-    
-    yield (-Float::INFINITY...from), @less_than  unless ignore_flanks
-    bins.each_with_index do |bin_range, index|
-      yield bin_range, @histogram_counts[index]
-    end
-    yield (to..Float::INFINITY), @greater_than  unless ignore_flanks
-  end
-  # def size; bins.size; end
 
-  def initialize(from, to, step)
-    @from, @to, @step = from, to, step
+  # from and to are values from initial range
+  # value_converter is used to convert both initial range boundaries and each value, which is put into a distribution
+  def initialize(from, to, step, &value_converter)
+    if block_given?
+      @value_converter = value_converter
+    else
+      @value_converter = ->(val) { val }
+    end
+    @from, @to = [@value_converter.call(from), @value_converter.call(to)].sort
+    @step = step
     @histogram_counts = Array.new(bins.size, 0)
-    
+
     @less_than = 0
     @greater_than = 0
 
@@ -26,11 +22,35 @@ class Histogram
     @elements_total_in_range = 0
   end
 
+  def range
+    @range ||= (from...to)
+  end
+
+  def bins
+    range.step(step).map{|left|
+      (left...[left + step, to].min)
+    }
+  end
+
+  def each_bin(ignore_flanks: false)
+    return enum_for(:each_bin, ignore_flanks: ignore_flanks)  unless block_given?
+
+    yield (-Float::INFINITY...from), @less_than  unless ignore_flanks
+    bins.each_with_index do |bin_range, index|
+      yield bin_range, @histogram_counts[index]
+    end
+    yield (to..Float::INFINITY), @greater_than  unless ignore_flanks
+  end
+
+  # def size; bins.size; end
+
+
   def in_range?(value)
     range.include?(value)
   end
 
   def bin_index(value)
+    value = @value_converter.call(value)
     return :less if value < from
     return :greater if value >= to
     ((value - from) / step).floor
@@ -44,7 +64,7 @@ class Histogram
       @less_than
     when :greater
       @greater_than
-    else 
+    else
       @histogram_counts[index]
     end
   end
