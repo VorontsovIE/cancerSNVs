53f69a4814172c13b72e33408f130496edf7f875
prijutme4ty <prijutme4ty@gmail.com>
2014-12-28 01:34:52 +0300

    refactor and improve SNV shuffling

--- lib/sequence_with_snp.rb
+++ lib/sequence_with_snp.rb
@@ -23,11 +23,24 @@ class SequenceWithSNP
   def revcomp
     SequenceWithSNP.new(Sequence.revcomp(right),
                         allele_variants.map{|letter| Sequence.complement(letter) },
-                        Sequence.revcomp(left))
+                        Sequence.revcomp(left), name: name)
   end
 
   # main variant (or allele_variant_number variant) context
   def context(before: 1, after: 1, allele_variant_number: 0)
     left[-before..-1] + allele_variants[allele_variant_number] + right[0, after]
+  end
+
+  def shuffle_string(str)
+    str.each_char.to_a.shuffle.join
+  end
+  private :shuffle_string
+
+  def shuffle_flanks(name: nil)
+    SequenceWithSNP.new(shuffle_string(left), allele_variants, shuffle_string(right), name: name)
+  end
+
+  def to_s
+    "#{name}\t#{left}[#{allele_variants.join('/')}]#{right}"
   end
 end
