77ca84c67e69a9092c6b42e0f5dd366508995530
prijutme4ty <prijutme4ty@gmail.com>
2014-12-28 19:05:21 +0300

    refine calculate_motif_statistics

--- calculate_motif_statistics.rb
+++ calculate_motif_statistics.rb
@@ -3,20 +3,42 @@ require 'site_info'
 require 'support'
 require 'optparse'
 
+mode = :all # by default consider not only disrupted/created sites, but all sites
 fold_change_cutoff = 5
-mode = :all
+
+site_before_substitution = false # by default don't consider whether site was before substition
+pvalue_before_cutoff = 0.0005
+
+site_after_substitution = false # by default don't consider whether site is after substition
+pvalue_after_cutoff = 0.0005
+
 OptionParser.new do |opts|
-  opts.banner = "Usage: #{opts.program_name} <sites infos> [options]"
-  opts.on('--all', 'Calculate motif statistics for all sites') {
-    mode = :all
+  opts.banner = "Usage: #{opts.program_name} <sites infos> [options]\n" +
+                "Calculate motif statistics for a list of site infos affected by SNVs.\n" +
+                "By default it considers all sites in a list."
+
+  opts.on('--site-before [PVALUE_CUTOFF]', 'Consider only sites with P-value before substitution less than cutoff.',
+                                          '(default cutoff = 0.0005)') {|value|
+    site_before_substitution = true
+    pvalue_before_cutoff = value.to_f  if value
   }
-  opts.on('--disrupted [FOLD_CHANGE]', 'Calculate motif statistics for disrupted sites (default fold change cutoff = 5)') {|value|
-    fold_change_cutoff = value.to_f
+
+  opts.on('--site-after [PVALUE_CUTOFF]', 'Consider only sites with P-value after substitution less than cutoff.',
+                                          '(default cutoff = 0.0005') {|value|
+    site_after_substitution = true
+    pvalue_after_cutoff = value.to_f  if value
+  }
+
+  opts.on('--disrupted [FOLD_CHANGE]', 'Consider only sites which were disrupted by an SNV',
+                                      '(default fold change cutoff = 5)') {|value|
     mode = :disrupted
+    fold_change_cutoff = value.to_f  if value
   }
-  opts.on('--emerged [FOLD_CHANGE]', 'Calculate motif statistics for emerged sites (default fold change cutoff = 5)') {|value|
-    fold_change_cutoff = value.to_f
+
+  opts.on('--emerged [FOLD_CHANGE]', 'Consider only sites which were emerged by an SNV',
+                                    '(default fold change cutoff = 5)') {|value|
     mode = :emerged
+    fold_change_cutoff = value.to_f  if value
   }
 end.parse!(ARGV)
 
@@ -24,17 +46,34 @@ mutated_site_infos_filename = ARGV[0] # source_data/sites_cancer_cpg.txt
 
 raise 'Specify input file with mutation infos'  unless mutated_site_infos_filename
 
-if mode == :disrupted
-  selector = ->(site) { site.site_before_substitution? && site.disrupted?(fold_change_cutoff: fold_change_cutoff) }
-elsif mode == :emerged
-  selector = ->(site) { site.site_after_substitution? && site.emerged?(fold_change_cutoff: fold_change_cutoff) }
-elsif mode == :all
-  selector = ->(site) { true }
+if site_before_substitution
+  site_before_checker = ->(site) { site.site_before_substitution?(pvalue_cutoff: pvalue_before_cutoff) }
+else
+  site_before_checker = ->(site) { true }
+end
+
+if site_after_substitution
+  site_after_checker = ->(site) { site.site_after_substitution?(pvalue_cutoff: pvalue_after_cutoff) }
+else
+  site_after_checker = ->(site) { true }
+end
+
+case mode
+when :disrupted
+  fold_change_checker = ->(site) { site.disrupted?(fold_change_cutoff: fold_change_cutoff) }
+when :emerged
+  fold_change_checker = ->(site) { site.emerged?(fold_change_cutoff: fold_change_cutoff) }
+when :all
+  fold_change_checker = ->(site) { true }
 else
   raise LogicError, 'Should never be here'
 end
 
-mutated_sites = MutatatedSiteInfo.each_site(mutated_site_infos_filename).select(&selector).map(&:motif_name)
+mutated_sites = MutatatedSiteInfo.each_site(mutated_site_infos_filename)
+                                .select(&site_before_checker)
+                                .select(&site_after_checker)
+                                .select(&fold_change_checker)
+                                .map(&:motif_name)
 
 motif_names = File.readlines('source_data/motif_names.txt').map(&:strip)
 
--- make_all.sh
+++ make_all.sh
@@ -24,23 +24,33 @@ ruby bin/preparations/filter_mutations.rb source_data/sites_random_wo_repeats.tx
 ruby bin/preparations/filter_mutations.rb source_data/sites_random_wo_repeats.txt --contexts NCG --mutation-types promoter,intronic > source_data/sites_random_cpg.txt
 
 
-# 35 sec each
+# 2 min
 ruby fitting_random_sites.rb source_data/sites_cancer_cpg.txt source_data/sites_random_cpg.txt > source_data/sites_random_fitted_cpg.txt 2> >(tee source_data/sites_random_fitted_cpg.log >&2)
 ruby fitting_random_sites.rb source_data/sites_cancer_tpc.txt source_data/sites_random_tpc.txt > source_data/sites_random_fitted_tpc.txt 2> >(tee source_data/sites_random_fitted_tpc.log >&2)
 
 # ~1 minute
 mkdir -p results/motif_statistics
 
-ruby calculate_motif_statistics.rb source_data/sites_cancer_cpg.txt --all > results/motif_statistics/cancer_cpg_all.txt
-ruby calculate_motif_statistics.rb source_data/sites_cancer_cpg.txt --emerged 5 > results/motif_statistics/cancer_cpg_emerged.txt
-ruby calculate_motif_statistics.rb source_data/sites_cancer_cpg.txt --disrupted 5 > results/motif_statistics/cancer_cpg_disrupted.txt
-ruby calculate_motif_statistics.rb source_data/sites_cancer_tpc.txt --all > results/motif_statistics/cancer_tpc_all.txt
-ruby calculate_motif_statistics.rb source_data/sites_cancer_tpc.txt --emerged 5 > results/motif_statistics/cancer_tpc_emerged.txt
-ruby calculate_motif_statistics.rb source_data/sites_cancer_tpc.txt --disrupted 5 > results/motif_statistics/cancer_tpc_disrupted.txt
+ruby calculate_motif_statistics.rb source_data/sites_cancer_cpg.txt > results/motif_statistics/cancer_cpg_all.txt
+ruby calculate_motif_statistics.rb source_data/sites_cancer_cpg.txt --site-after 0.0005 --emerged 5 > results/motif_statistics/cancer_cpg_sites_emerged.txt
+ruby calculate_motif_statistics.rb source_data/sites_cancer_cpg.txt --site-before 0.0005 --disrupted 5 > results/motif_statistics/cancer_cpg_sites_disrupted.txt
+ruby calculate_motif_statistics.rb source_data/sites_cancer_cpg.txt --site-after 0.0005 > results/motif_statistics/cancer_cpg_sites_after.txt
+ruby calculate_motif_statistics.rb source_data/sites_cancer_cpg.txt --site-before 0.0005 > results/motif_statistics/cancer_cpg_sites_before.txt
 
-ruby calculate_motif_statistics.rb source_data/sites_random_fitted_cpg.txt --all > results/motif_statistics/random_fitted_cpg_all.txt
-ruby calculate_motif_statistics.rb source_data/sites_random_fitted_cpg.txt --emerged 5 > results/motif_statistics/random_fitted_cpg_emerged.txt
-ruby calculate_motif_statistics.rb source_data/sites_random_fitted_cpg.txt --disrupted 5 > results/motif_statistics/random_fitted_cpg_disrupted.txt
-ruby calculate_motif_statistics.rb source_data/sites_random_fitted_tpc.txt --all > results/motif_statistics/random_fitted_tpc_all.txt
-ruby calculate_motif_statistics.rb source_data/sites_random_fitted_tpc.txt --emerged 5 > results/motif_statistics/random_fitted_tpc_emerged.txt
-ruby calculate_motif_statistics.rb source_data/sites_random_fitted_tpc.txt --disrupted 5 > results/motif_statistics/random_fitted_tpc_disrupted.txt
+ruby calculate_motif_statistics.rb source_data/sites_cancer_tpc.txt > results/motif_statistics/cancer_tpc_all.txt
+ruby calculate_motif_statistics.rb source_data/sites_cancer_tpc.txt --site-after 0.0005 --emerged 5 > results/motif_statistics/cancer_tpc_sites_emerged.txt
+ruby calculate_motif_statistics.rb source_data/sites_cancer_tpc.txt --site-before 0.0005 --disrupted 5 > results/motif_statistics/cancer_tpc_sites_disrupted.txt
+ruby calculate_motif_statistics.rb source_data/sites_cancer_tpc.txt --site-after 0.0005 > results/motif_statistics/cancer_tpc_sites_after.txt
+ruby calculate_motif_statistics.rb source_data/sites_cancer_tpc.txt --site-before 0.0005 > results/motif_statistics/cancer_tpc_sites_before.txt
+
+ruby calculate_motif_statistics.rb source_data/sites_random_fitted_cpg.txt > results/motif_statistics/random_fitted_cpg_all.txt
+ruby calculate_motif_statistics.rb source_data/sites_random_fitted_cpg.txt --site-after 0.0005 --emerged 5 > results/motif_statistics/random_fitted_cpg_sites_emerged.txt
+ruby calculate_motif_statistics.rb source_data/sites_random_fitted_cpg.txt --site-before 0.0005 --disrupted 5 > results/motif_statistics/random_fitted_cpg_sites_disrupted.txt
+ruby calculate_motif_statistics.rb source_data/sites_random_fitted_cpg.txt --site-after 0.0005 > results/motif_statistics/random_fitted_cpg_sites_after.txt
+ruby calculate_motif_statistics.rb source_data/sites_random_fitted_cpg.txt --site-before 0.0005 > results/motif_statistics/random_fitted_cpg_sites_before.txt
+
+ruby calculate_motif_statistics.rb source_data/sites_random_fitted_tpc.txt > results/motif_statistics/random_fitted_tpc_all.txt
+ruby calculate_motif_statistics.rb source_data/sites_random_fitted_tpc.txt --site-after 0.0005 --emerged 5 > results/motif_statistics/random_fitted_tpc_sites_emerged.txt
+ruby calculate_motif_statistics.rb source_data/sites_random_fitted_tpc.txt --site-before 0.0005 --disrupted 5 > results/motif_statistics/random_fitted_tpc_sites_disrupted.txt
+ruby calculate_motif_statistics.rb source_data/sites_random_fitted_tpc.txt --site-after 0.0005 > results/motif_statistics/random_fitted_tpc_sites_after.txt
+ruby calculate_motif_statistics.rb source_data/sites_random_fitted_tpc.txt --site-before 0.0005 > results/motif_statistics/random_fitted_tpc_sites_before.txt
