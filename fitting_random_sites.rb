19a163f8c637e4036b2d13527ae52ac94535154f
prijutme4ty <prijutme4ty@gmail.com>
2014-12-28 19:06:12 +0300

    fitting shouldn't throw away non-sites

--- fitting_random_sites.rb
+++ fitting_random_sites.rb
@@ -10,15 +10,15 @@ motif_names = File.readlines('source_data/motif_names.txt').map(&:strip)
 mutated_site_infos_cancer_filename = ARGV[0] # 'source_data/sites_cancer_cpg.txt'
 mutated_site_infos_random_filename = ARGV[1] # 'source_data/sites_random_cpg.txt'
 
-fitters = MotifHistogramFitter.new(motif_names, Histogram.new(1e-7, 0.0005, 1.0){|pvalue| - Math.log2(pvalue) })
+fitters = MotifHistogramFitter.new(motif_names, Histogram.new(1e-7, 1, 1.0){|pvalue| - Math.log2(pvalue) })
 
-MutatatedSiteInfo.each_site(mutated_site_infos_cancer_filename).select(&:site_before_substitution?).each do |mutated_site_info|
+MutatatedSiteInfo.each_site(mutated_site_infos_cancer_filename).each do |mutated_site_info|
   fitters.add_element(mutated_site_info.motif_name, mutated_site_info.pvalue_1)
 end
 
 $stderr.puts "Loaded #{fitters.goal_total} original sites"
 
-MutatatedSiteInfo.each_site(mutated_site_infos_random_filename).select(&:site_before_substitution?).each do |mutated_site_info|
+MutatatedSiteInfo.each_site(mutated_site_infos_random_filename).each do |mutated_site_info|
   fitters.fit_element(mutated_site_info.motif_name, mutated_site_info.pvalue_1) do
     puts mutated_site_info.line
   end
