9eeb623f815c8f513eee8f2bfce7e5af56b31eac
prijutme4ty <prijutme4ty@gmail.com>
2015-01-23 03:15:09 +0300

    SequenceWithSNP: added ability to compare them; #subsequence, #sequence_variant, conversion to pyrimidine context; constructor error messages

--- lib/sequence_with_snp.rb
+++ lib/sequence_with_snp.rb
@@ -3,10 +3,10 @@ require_relative 'sequence'
 class SequenceWithSNP
   attr_reader :left, :allele_variants, :right, :name
   def initialize(left, allele_variants, right, options = {})
-    raise unless Sequence.valid_sequence?(left)
-    raise unless Sequence.valid_sequence?(right)
-    raise unless allele_variants.all?{|letter| %w[A C G T].include?(letter.upcase) }
-    @left, @allele_variants, @right = left, allele_variants, right
+    raise "SequenceWithSNP left part is invalid: #{left}" unless Sequence.valid_sequence?(left)
+    raise "SequenceWithSNP right part is invalid: #{right}" unless Sequence.valid_sequence?(right)
+    raise "SequenceWithSNP allele_variants are invalid: #{allele_variants}" unless allele_variants.map(&:to_s).all?{|letter| %w[A C G T].include?(letter.upcase) }
+    @left, @allele_variants, @right = left, allele_variants.map(&:to_s), right
     @name = options[:name] || (left + '_' + allele_variants.join('-') + '_' + right)
   end
 
@@ -26,9 +26,37 @@ class SequenceWithSNP
                         Sequence.revcomp(left), name: name)
   end
 
+  def pyrimidine_context?
+    ['C', 'T'].include?(allele_variants.first.upcase)
+  end
+
+  def in_pyrimidine_context
+    pyrimidine_context? ? self : self.revcomp
+  end
+
+  def ==(other)
+    other.is_a?(SequenceWithSNP) && @left == other.left && @allele_variants == other.allele_variants && @right == other.right
+  end
+
+  def eql?(other)
+    other.class.equal?(SequenceWithSNP) && @left.eql?(other.left) && @allele_variants.eql?(other.allele_variants) && @right.eql?(other.right)
+  end
+
+  def hash
+    [@left, @allele_variants, @right].hash
+  end
+
   # main variant (or allele_variant_number variant) context
   def context(before: 1, after: 1, allele_variant_number: 0)
     left[-before..-1] + allele_variants[allele_variant_number] + right[0, after]
+  end
+
+  def subsequence(before:, after:)
+    SequenceWithSNP.new(left[-before..-1], allele_variants, right[0, after], name: name)
+  end
+
+  def sequence_variant(allele_variant_number)
+    "#{left}#{allele_variants[allele_variant_number]}#{right}"
   end
 
   def shuffle_string(str)
@@ -42,5 +70,9 @@ class SequenceWithSNP
 
   def to_s
     "#{name}\t#{left}[#{allele_variants.join('/')}]#{right}"
+  end
+
+  def inspect
+    to_s
   end
 end
