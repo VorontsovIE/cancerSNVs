9584bda9f54fd5a3b989b4c033bb1946ed63d081
prijutme4ty <prijutme4ty@gmail.com>
2015-01-24 17:34:03 +0300

    markup_mutation_types now discards mutation types (intronic markup in SNV file was not exact); loading change loading promoters to exon-file

--- bin/preparations/markup_mutations_in_promoters.rb
+++ bin/preparations/markup_mutations_in_promoters.rb
@@ -1,18 +1,13 @@
-require 'interval_notation'
 $:.unshift File.absolute_path('../../lib', __dir__)
 require 'breast_cancer_snv'
-require 'cage_peak'
-require 'repeat_masker_info'
-require 'ensembl_exon'
 require 'load_genome_structure'
 
-raise 'Specify gene TSS file' unless gene_tss_filename = ARGV[0] # './source_data/gene_tss.txt'
-raise 'Specify cage peaks' unless cage_peaks_filename = ARGV[1] # '/home/ilya/iogen/cages/hg19/freeze1/hg19.cage_peak_tpm_ann.osc.txt'
-raise 'Specify SNV infos' unless snvs_filename = ARGV[2] # './source_data/SNV_infos_original.txt'
-raise 'Specify repeat masker folder' unless genome_repeat_masker_folder = ARGV[3] # '/home/ilya/iogen/genome/hg19_repeatMasker'
-raise 'Specify ensembl exons markup' unless exons_filename = ARGV[4] # '/home/ilya/iogen/genome/hg19_exons(ensembl,GRCh37.p13).txt'
+raise 'Specify cage peaks' unless cage_peaks_filename = ARGV[0] # '/home/ilya/iogen/cages/hg19/freeze1/hg19.cage_peak_tpm_ann.osc.txt'
+raise 'Specify SNV infos' unless snvs_filename = ARGV[1] # './source_data/SNV_infos_original.txt'
+raise 'Specify repeat masker folder' unless genome_repeat_masker_folder = ARGV[2] # '/home/ilya/iogen/genome/hg19_repeatMasker'
+raise 'Specify ensembl exons markup' unless exons_filename = ARGV[3] # '/home/ilya/iogen/genome/hg19_exons(ensembl,GRCh37.p13).txt'
 
-promoters_by_chromosome = load_promoters_by_chromosome(gene_tss_filename, length_5_prime: 2000, length_3_prime: 500)
+promoters_by_chromosome = load_promoters_by_chromosome(exons_filename, length_5_prime: 2000, length_3_prime: 500)
 cage_peaks_by_chromosome = load_cage_peaks_by_chromosome(cage_peaks_filename, length_5_prime: 2000, length_3_prime: 500)
 coding_exons_by_chromosome = read_coding_exons_by_chromosome(exons_filename)
 introns_by_chromosome = read_introns_by_chromosome(exons_filename)
@@ -21,6 +16,7 @@ repeats_by_chromosome = read_repeats_by_chromosome(genome_repeat_masker_folder, 
 puts BreastCancerSNV::FILE_HEADER
 BreastCancerSNV.each_substitution_in_file(snvs_filename).each do |snv|
   chromosome = "chr#{snv.chr}".to_sym
+  snv.mut_types = [] # discard all mutation types
   if promoters_by_chromosome[chromosome].include_position?(snv.position)
     snv.mut_types << :promoter
   end
