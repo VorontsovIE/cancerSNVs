695ebc0cc565d6614dbe90c1245e28258f972aa2
prijutme4ty <prijutme4ty@gmail.com>
2015-01-24 17:47:22 +0300

    restore snvs from sequences; filter regulatory mutations (SNVs, not sequences)

---
+++ bin/preparations/create_snv_infos_by_genome_sequences.rb
@@ -0,0 +1,39 @@
+$:.unshift File.absolute_path('../../lib', __dir__)
+require 'breast_cancer_snv'
+
+raise 'Specify file with sequences'  unless sequences_filename = ARGV[0] # './source_data/sequences_background_genome.txt'
+
+puts BreastCancerSNV::FILE_HEADER
+File.readlines(sequences_filename).each.lazy.map {|line|
+  full_pos, seq = line.chomp.split("\t")
+  chr, pos = full_pos.split(':')
+  pos = pos.to_i
+  chr = chr.sub(/\Achr/, '')
+
+  seq_w_snv = SequenceWithSNP.from_string(seq)
+  seq_w_snv_pyrimidine = seq_w_snv.in_pyrimidine_context
+
+  short_seq_w_snv = seq_w_snv.subsequence(before: 10, after: 10)
+  short_seq_w_snv_pyrimidine = seq_w_snv_pyrimidine.subsequence(before: 10, after: 10)
+
+  BreastCancerSNV.new(full_pos,
+                      'random genome mutations', chr, pos, 'GRCh37',
+
+                      seq_w_snv.allele_variants[0], seq_w_snv.allele_variants[1],
+
+                      short_seq_w_snv_pyrimidine.left,
+                      seq_w_snv_pyrimidine.allele_variants[0],
+                      seq_w_snv_pyrimidine.allele_variants[1],
+                      short_seq_w_snv_pyrimidine.right,
+
+                      seq_w_snv.pyrimidine_context? ? '+' : '-',
+                      '', '', '', '', #:gene, :gene_id, :ccds_id, :transcript_id,
+
+                      '', Set.new, #:gene_type, :mut_types,
+
+                      '', '', '', # :mRNA_mut_syntax, :cds_mut_syntax, :aa_mut_syntax,
+                      '', '' # :current_conf_status, :validation_platform
+                    )
+}.each {|snv_info|
+  puts snv_info.to_s
+}
---
+++ bin/preparations/filter_snv_infos.rb
@@ -0,0 +1,24 @@
+$:.unshift File.absolute_path('../../lib', __dir__)
+require 'breast_cancer_snv'
+require 'load_genome_structure'
+require 'optparse'
+
+flank_length = 25
+
+OptionParser.new do |opts|
+  opts.banner = 'Usage: #{program_name} <SNVs file> <genome folder> [options]'
+  opts.separator 'Options:'
+  opts.on('--flank-length LENGTH') {|value| flank_length = value.to_i }
+end.parse!(ARGV)
+
+raise 'Specify file with SNV infos'  unless snvs_filename = ARGV[0] # './source_data/SNV_infos.txt'
+raise 'Specify genome folder'  unless genome_folder = ARGV[1] # './source_data/SNV_infos.txt'
+
+puts BreastCancerSNV::FILE_HEADER
+snvs = BreastCancerSNV.each_substitution_in_file(snvs_filename).select{|snv|
+  snv.promoter? || snv.intronic?
+}.to_a.uniq{|snv| # remove duplicates
+  snv.snp_sequence_from_genome(genome_folder, flank_length, flank_length, name: snv.variant_id).in_pyrimidine_context
+}.each{|snv|
+  puts snv
+}
