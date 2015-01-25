37a88956025a53a5f58d7df6eabd007d89500425
prijutme4ty <prijutme4ty@gmail.com>
2015-01-25 16:18:03 +0300

    refactor make_all

--- make_all.sh
+++ make_all.sh
@@ -13,13 +13,15 @@ SITES_RANDOM = ./results/intermediate/site_subsets/random_any.txt
 # ruby bin/preparations/filter_mutations.rb $SNV_INFOS ./source_data/sites_cancer.txt  --mutation-types promoter,intronic  >  $SITES_CANCER
 # ruby bin/preparations/filter_mutations.rb $SNV_INFOS ./source_data/sites_random.txt  --mutation-types promoter,intronic  >  $SITES_RANDOM
 
+ln ./source_data/sites_cancer.txt  $SITES_CANCER
+ln ./source_data/sites_random.txt  $SITES_RANDOM
+
+./make_all_for_context.sh  any  $SITES_CANCER  $SITES_RANDOM
 
 ruby bin/preparations/filter_mutations.rb $SNV_INFOS  $SITES_CANCER  --contexts TCN  --mutation-types promoter,intronic  >  ./results/intermediate/site_subsets/cancer_tpc.txt
-ruby bin/preparations/filter_mutations.rb $SNV_INFOS  $SITES_CANCER  --contexts NCG  --mutation-types promoter,intronic  >  ./results/intermediate/site_subsets/cancer_cpg.txt
-
 ruby bin/preparations/filter_mutations.rb $SNV_INFOS  $SITES_RANDOM  --contexts TCN  --mutation-types promoter,intronic  >  ./results/intermediate/site_subsets/random_tpc.txt
-ruby bin/preparations/filter_mutations.rb $SNV_INFOS  $SITES_RANDOM  --contexts NCG  --mutation-types promoter,intronic  >  ./results/intermediate/site_subsets/random_cpg.txt
-
-./make_all_for_context.sh  any  ./results/intermediate/site_subsets/cancer_any.txt  ./results/intermediate/site_subsets/random_any.txt
-./make_all_for_context.sh  cpg  ./results/intermediate/site_subsets/cancer_cpg.txt  ./results/intermediate/site_subsets/random_cpg.txt
 ./make_all_for_context.sh  tpc  ./results/intermediate/site_subsets/cancer_tpc.txt  ./results/intermediate/site_subsets/random_tpc.txt
+
+ruby bin/preparations/filter_mutations.rb $SNV_INFOS  $SITES_CANCER  --contexts NCG  --mutation-types promoter,intronic  >  ./results/intermediate/site_subsets/cancer_cpg.txt
+ruby bin/preparations/filter_mutations.rb $SNV_INFOS  $SITES_RANDOM  --contexts NCG  --mutation-types promoter,intronic  >  ./results/intermediate/site_subsets/random_cpg.txt
+./make_all_for_context.sh  cpg  ./results/intermediate/site_subsets/cancer_cpg.txt  ./results/intermediate/site_subsets/random_cpg.txt
