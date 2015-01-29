d3c1d196a2349f710f1e56c537ff1ea17a8c22a9
prijutme4ty <prijutme4ty@gmail.com>
2015-01-29 14:10:53 +0300

    gene by motif should return only approved symbol, not all aliases

--- bin/supplementary/target_genes/genes_by_motifs.rb
+++ bin/supplementary/target_genes/genes_by_motifs.rb
@@ -1,5 +1,5 @@
-genes_by_motif = File.readlines('./source_data/hocomoco_genes_more_info.csv').drop(1)
+genes_by_motif = File.readlines('./source_data/hocomoco_genes_infos.csv').drop(1)
                       .map{|line| line.chomp.split("\t").first(2) }
-                      .map{|motif,genes| [motif, genes.split] }.to_h
+                      .map{|motif,genes| [motif, genes.split.first] }.to_h
 
 puts ARGF.read.split.flat_map{|motif| genes_by_motif[motif] }
