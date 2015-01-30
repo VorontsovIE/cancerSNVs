b46983f5d1214fb39b4c6081b44939a3198f026e
prijutme4ty <prijutme4ty@gmail.com>
2015-01-30 05:04:28 +0300

    remove dependency from Fantom5 CAGE TSS-peaks

--- bin/preparations/snv_markup.rb
+++ bin/preparations/snv_markup.rb
@@ -3,12 +3,9 @@ require 'breast_cancer_snv'
 require 'load_genome_structure'
 
 raise 'Specify SNV infos' unless snvs_filename = ARGV[0] # './source_data/SNV_infos_original.txt'
-raise 'Specify cage peaks' unless cage_peaks_filename = ARGV[1] # '/home/ilya/iogen/cages/hg19/freeze1/hg19.cage_peak_tpm_ann.osc.txt'
-raise 'Specify ensembl exons markup' unless exons_filename = ARGV[2] # '/home/ilya/iogen/genome/hg19_exons(ensembl,GRCh37.p13).txt'
+raise 'Specify ensembl exons markup' unless exons_filename = ARGV[1] # '/home/ilya/iogen/genome/hg19_exons(ensembl,GRCh37.p13).txt'
 
 promoters_by_chromosome = load_promoters_by_chromosome(exons_filename, length_5_prime: 2000, length_3_prime: 500)
-cage_peaks_by_chromosome = load_cage_peaks_by_chromosome(cage_peaks_filename, length_5_prime: 2000, length_3_prime: 500)
-coding_exons_by_chromosome = read_coding_exons_by_chromosome(exons_filename)
 introns_by_chromosome = read_introns_by_chromosome(exons_filename)
 
 puts BreastCancerSNV::FILE_HEADER
@@ -21,14 +18,6 @@ BreastCancerSNV.each_substitution_in_file(snvs_filename).each do |snv|
 
   if introns_by_chromosome[chromosome].include_position?(snv.position)
     snv.mut_types << :intronic
-  end
-
-  if cage_peaks_by_chromosome[chromosome].include_position?(snv.position)
-    snv.mut_types << :cage_peak
-  end
-
-  if coding_exons_by_chromosome[chromosome].include_position?(snv.position)
-    snv.mut_types << :exon_coding
   end
 
   puts snv
