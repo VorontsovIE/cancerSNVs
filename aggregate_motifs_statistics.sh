35aa2701432f38c68006ecb272a134c21223be9d
prijutme4ty <prijutme4ty@gmail.com>
2015-01-30 06:32:17 +0300

    not only subjected from disrupted but subjected to/protected from disruption/emergence

--- aggregate_motifs_statistics.sh
+++ aggregate_motifs_statistics.sh
@@ -9,36 +9,38 @@ MOTIF_INFOS=./source_data/hocomoco_genes_infos.csv
 mkdir -p $MOTIF_STATISTICS_FOLDER/filtered
 
 for CONTEXT in any cpg tpc; do
-  mkdir -p  ${MOTIF_STATISTICS_FOLDER}/full/${CONTEXT}  ${MOTIF_STATISTICS_FOLDER}/filtered/${CONTEXT}
-  
+  mkdir -p  ${MOTIF_STATISTICS_FOLDER}/full/${CONTEXT}
+
   for RANDOM_VARIANT  in  random_shuffle_135  random_shuffle_137  random_shuffle_139  random_genome_13  random_genome_15  random_genome_17; do
     ruby summary.rb   ${MOTIF_STATISTICS_FOLDER}/slices/${CONTEXT}/cancer  \
                       ${MOTIF_STATISTICS_FOLDER}/slices/${CONTEXT}/${RANDOM_VARIANT}  \
                       $MOTIF_NAMES  $MOTIF_INFOS --correction fdr  \
                       >  ${MOTIF_STATISTICS_FOLDER}/full/${CONTEXT}/${RANDOM_VARIANT}.csv
 
-    ruby filter_summary.rb  ${MOTIF_STATISTICS_FOLDER}/full/${CONTEXT}/${RANDOM_VARIANT}.csv  \
-                            --motif-qualities A,B,C,D  --significance 0.05  \
-                            >  ${MOTIF_STATISTICS_FOLDER}/filtered/${CONTEXT}/${RANDOM_VARIANT}.csv
+    for SUBJECTED_OR_PROTECTED  in  subjected  protected; do
+      for DISRUPTION_OR_EMERGENCE  in  disruption  emergence; do
+        mkdir -p ${MOTIF_STATISTICS_FOLDER}/filtered/${SUBJECTED_OR_PROTECTED}/${DISRUPTION_OR_EMERGENCE}/${CONTEXT}
+        ruby filter_summary.rb  ${MOTIF_STATISTICS_FOLDER}/full/${CONTEXT}/${RANDOM_VARIANT}.csv  \
+                                --motif-qualities A,B,C,D  --significance 0.05  \
+                                --${DISRUPTION_OR_EMERGENCE}  --${SUBJECTED_OR_PROTECTED}  \
+                                >  ${MOTIF_STATISTICS_FOLDER}/filtered/${SUBJECTED_OR_PROTECTED}/${DISRUPTION_OR_EMERGENCE}/${CONTEXT}/${RANDOM_VARIANT}.csv
+      done
+    done
   done
 done
 
 for CONTEXT in any cpg tpc; do
-  mkdir -p  ${MOTIF_STATISTICS_FOLDER}/common_motifs/${CONTEXT}
-  
-  SHUFFLE_FILTERED_RESULTS_FILES=""
-  for RANDOM_VARIANT  in  random_shuffle_135  random_shuffle_137  random_shuffle_139; do
-    SHUFFLE_FILTERED_RESULTS_FILES="${SHUFFLE_FILTERED_RESULTS_FILES}  ${MOTIF_STATISTICS_FOLDER}/filtered/${CONTEXT}/${RANDOM_VARIANT}.csv"
+  for SUBJECTED_OR_PROTECTED  in  subjected  protected; do
+    for DISRUPTION_OR_EMERGENCE  in  disruption  emergence; do
+
+      FILTERED_FOLDER=${MOTIF_STATISTICS_FOLDER}/filtered/${SUBJECTED_OR_PROTECTED}/${DISRUPTION_OR_EMERGENCE}/${CONTEXT}
+      COMMON_MOTIFS_FOLDER=${MOTIF_STATISTICS_FOLDER}/common_motifs/${SUBJECTED_OR_PROTECTED}/${DISRUPTION_OR_EMERGENCE}/${CONTEXT}
+
+      mkdir -p  ${COMMON_MOTIFS_FOLDER}
+
+      ls  ${FILTERED_FOLDER}/random_genome_*.csv | ruby common_motifs.rb  >  ${COMMON_MOTIFS_FOLDER}/compared_to_each_genome.txt
+      ls  ${FILTERED_FOLDER}/random_shuffle_*.csv | ruby common_motifs.rb  >  ${COMMON_MOTIFS_FOLDER}/compared_to_each_shuffle.txt
+      ls  ${FILTERED_FOLDER}/random_*.csv | ruby common_motifs.rb  >  ${COMMON_MOTIFS_FOLDER}/compared_to_each.txt
+    done
   done
-
-  GENOME_FILTERED_RESULTS_FILES=""
-  for RANDOM_VARIANT  in  random_genome_13  random_genome_15  random_genome_17; do
-    GENOME_FILTERED_RESULTS_FILES="${GENOME_FILTERED_RESULTS_FILES}  ${MOTIF_STATISTICS_FOLDER}/filtered/${CONTEXT}/${RANDOM_VARIANT}.csv"
-  done
-
-  ALL_FILTERED_RESULTS="${GENOME_FILTERED_RESULTS_FILES}  ${SHUFFLE_FILTERED_RESULTS_FILES}"
-
-  ruby common_motifs.rb ${GENOME_FILTERED_RESULTS_FILES}  >  ${MOTIF_STATISTICS_FOLDER}/common_motifs/${CONTEXT}/compared_to_each_genome.txt
-  ruby common_motifs.rb ${SHUFFLE_FILTERED_RESULTS_FILES}  >  ${MOTIF_STATISTICS_FOLDER}/common_motifs/${CONTEXT}/compared_to_each_shuffle.txt
-  ruby common_motifs.rb ${ALL_FILTERED_RESULTS}  >  ${MOTIF_STATISTICS_FOLDER}/common_motifs/${CONTEXT}/compared_to_each.txt
 done
--- filter_summary.rb
+++ filter_summary.rb
@@ -7,12 +7,21 @@ end
 motif_qualities = [:A, :B, :C]
 significance_cutoff = 0.05
 
+subjected_or_protected = :subjected
+disruption_or_emergence = :disruption
+
 OptionParser.new do |opts|
   opts.banner = "Usage: #{opts.program_name} <file to filter> [options]"
   opts.separator('Options:')
   opts.on('--motif-qualities QUALITIES', 'Take motif of given qualities (comma-separated). Default is A,B,C') {|value|
     motif_qualities = value.upcase.split(',').map(&:to_sym)
   }
+
+  opts.on('--subjected', 'Select only sites subjected to site disruption/emergence.', 'Don\'t use with --protected'){ subjected_or_protected = :subjected }
+  opts.on('--protected', 'Select only sites protected from site disruption/emergence.', 'Don\'t use with --subjected'){ subjected_or_protected = :protected }
+
+  opts.on('--disruption', 'Select only sites subjected or protected to site disruption.', 'Don\'t use with --emergence'){ disruption_or_emergence = :disruption }
+  opts.on('--emergence', 'Select only sites subjected or protected to site emergence.', 'Don\'t use with --disruption'){ disruption_or_emergence = :emergence }
 
   opts.on('--significance CUTOFF', 'Significance cutoff. Default is 0.05') {|value|
     significance_cutoff = value.to_f
@@ -23,7 +32,7 @@ lines = ARGF.readlines
 
 puts lines.shift
 
-lines.map{|line|
+motif_infos = lines.map{|line|
   motif, official_gene_name, quality,
           cancer_to_random_disruption_ratio, disruption_significance,
           cancer_to_random_emergence_ratio, emergence_significance,
@@ -48,13 +57,34 @@ lines.map{|line|
     emergence_significance_uncorrected: to_float(emergence_significance_uncorrected),
     gene: gene,
   }
+}
+
+if disruption_or_emergence == :disruption
+  characteristic = :cancer_to_random_disruption_ratio
+  characteristic_significance = :disruption_significance
+else  # emergence
+  characteristic = :cancer_to_random_emergence_ratio
+  characteristic_significance = :emergence_significance
+end
+
+filtered_motif_infos = motif_infos.select{|infos|
+  infos[characteristic]
 }.select{|infos|
-  infos[:cancer_to_random_disruption_ratio] && infos[:cancer_to_random_disruption_ratio] > 1
+  if subjected_or_protected == :subjected
+     infos[characteristic] > 1
+  else # protected
+    infos[characteristic] < 1
+  end
 }.select{|infos|
-  infos[:disruption_significance] && infos[:disruption_significance] < significance_cutoff # ||
-  # infos[:emergence_significance] && infos[:emergence_significance] < significance_cutoff
+  infos[characteristic_significance] && infos[characteristic_significance] < significance_cutoff
 }.select{|infos|
   motif_qualities.include?(infos[:quality])
-}.sort_by{|infos| infos[:cancer_to_random_disruption_ratio] }.reverse.each{|infos|
+}.sort_by{|infos|
+  infos[characteristic]
+}
+
+filtered_motif_infos.reverse!  if subjected_or_protected == :subjected
+
+filtered_motif_infos.each{|infos|
   puts infos[:line]
 }
