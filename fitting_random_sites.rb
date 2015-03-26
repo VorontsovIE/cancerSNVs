5ddc88c554f8e684292015e5ac657c7c6111e5a8
prijutme4ty <prijutme4ty@gmail.com>
2015-03-26 23:14:36 +0300

    fitting should be context-aware

--- fitting_random_sites.rb
+++ fitting_random_sites.rb
@@ -3,45 +3,56 @@
 $:.unshift File.absolute_path('lib', __dir__)
 require 'histogram'
 require 'site_info'
-require 'motifwise_histogram_fitting'
+require 'breast_cancer_snv'
+require 'multi_histogram_fitter'
 require 'optparse'
+
+def context_by_snv_name(snv_infos_filename)
+  results = {}
+
+  BreastCancerSNV
+    .each_substitution_in_file(snv_infos_filename)
+    .map{|snv|
+      [snv.variant_id, snv.snp_sequence_pyrimidine_context(five_prime_flank_length: 1, three_prime_flank_length: 1)]
+    }.to_h
+end
 
 fitting_fold = 1
 OptionParser.new do |opts|
-  opts.banner = "Usage: #{opts.program_name} <original sites (cancer)> <sites to be fitted (random)> [options]\n" +
+  opts.banner = "Usage: #{opts.program_name} <original sites (cancer)> <sites to be fitted (random)> <SNV infos cancer> <SNV infos random> [options]\n" +
                 "Calculates P-value distribution for each site in cancer and takes a subset of random sites\n" +
                 "so that their P-value distributions fit original distributions."
   opts.on('--fold N', 'Take N times more sites (dilate histogram of distribution N times)') {|value|
-    fitting_fold = value.to_f
+    fitting_fold = value.to_i
   }
 end.parse!(ARGV)
 
-mutated_site_infos_cancer_filename = ARGV[0] # './results/intermediate/site_subsets/cancer_cpg.txt'
-mutated_site_infos_random_filename = ARGV[1] # './results/intermediate/site_subsets/random_cpg.txt'
+raise 'Specify file with mutated sites in cancer'  unless mutated_site_infos_cancer_filename = ARGV[0] # './results/intermediate/site_subsets/cancer_cpg.txt'
+raise 'Specify file with mutated sites in control group'  unless mutated_site_infos_random_filename = ARGV[1] # './results/intermediate/site_subsets/random_cpg.txt'
+raise 'Specify file with SNV infos for cancer'  unless cancer_snv_sequences_file = ARGV[2] # ./results/SNVs/SNV_infos_cancer.txt
+raise 'Specify file with SNV infos for control group'  unless random_snv_sequences_file = ARGV[3] # ./results/SNVs/SNV_infos_random_genome_13.txt
 
-raise 'Specify cancer and random site files'  unless mutated_site_infos_cancer_filename && mutated_site_infos_random_filename
+cancer_snv_contexts = context_by_snv_name(cancer_snv_sequences_file)
+random_snv_contexts = context_by_snv_name(random_snv_sequences_file)
 
-histograms_for_motifs = Hash.new{|hsh, motif_name|
-  hsh[motif_name] = Histogram.new(1e-7, 1, 1.0){|pvalue| - Math.log2(pvalue)}
+histograms = MultiHistogram.new{
+  Histogram.new(-2, 2, 4){|pvalue_1| pvalue_1 }
 }
 
 MutatatedSiteInfo.each_site(mutated_site_infos_cancer_filename).each do |site|
-  histograms_for_motifs[site.motif_name].add_element(site.pvalue_1)
+  motif = site.motif_name
+  context = cancer_snv_contexts[ site.normalized_snp_name ]
+  histograms.add_element([motif, context], site.pvalue_1)
 end
 
-
-fitters_for_motifs = histograms_for_motifs.map{|motif, histogram|
-  goal_histogram = histogram.multiply(fitting_fold)
-  [motif, HistogramFitting.new(goal_histogram)]
-}.to_h
-
-fitters = MotifHistogramFitter.new(fitters_for_motifs, raise_on_missing: false)
+fitters = histograms.multiply(fitting_fold).fitter(raise_on_missing: false)
 
 $stderr.puts "Loaded #{fitters.goal_total} original sites"
 
 MutatatedSiteInfo.each_site(mutated_site_infos_random_filename).each_with_index do |site, index|
-  # $stderr.puts "#{index} sites processed"  if index % 100000 == 0
-  fitters.fit_element(site.motif_name, site.pvalue_1) do
+  motif = site.motif_name
+  context = random_snv_contexts[ site.normalized_snp_name ]
+  fitters.fit_element([motif, context], site.pvalue_1) do
     puts site.line
   end
 end
