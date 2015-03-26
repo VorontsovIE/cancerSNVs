390ff914221c26bbf3ab8df7836c5e7817657351
prijutme4ty <prijutme4ty@gmail.com>
2015-03-26 16:08:56 +0300

    some WIP for refactoring RateComparisonInfos

--- lib/rate_comparison_infos.rb
+++ lib/rate_comparison_infos.rb
@@ -2,6 +2,63 @@ def to_float(str)
   str && str.to_f
 end
 
+# # unclassified is not included in total
+# FischerTableData = Struct.new(:first_total, :first_positive, :second_total, :second_positive, :first_unclassified, :second_unclassified) do
+#   def first_negative
+#     first_total - first_positive
+#   end
+#   def second_negative
+#     second_total - second_positive
+#   end
+
+#   def first_rate
+#     first_positive.to_f / first_total
+#   end
+#   def second_rate
+#     second_positive.to_f / second_total
+#   end
+#   def first_to_second_ratio
+#     first_rate / second_rate
+#   end
+
+#   def significance # aware of unclassified items
+#     PvalueCalculator
+#       .new(class_counts: :class_and_total)
+#       .calculate(
+#         [first_positive, second_positive, first_total, second_total],
+#         unclassified_a: first_unclassified,
+#         unclassified_b: second_unclassified
+#       )
+#   end
+# end
+
+# RateComparisonInfo_2 = Struct.new(
+#   :motif,
+#   :disruption_rates,
+#   :emergence_rates,
+#   :official_gene_name, :quality, :gene
+# ) do
+
+#   def self.from_string(line)
+#     motif, official_gene_name, quality,
+#     cancer_to_random_disruption_ratio, disruption_significance_fitting_aware,
+#     cancer_to_random_emergence_ratio, emergence_significance_fitting_aware,
+#     random_disrupted, random_emerged, random_total_before_substitution, random_total_after_substitution,
+#     cancer_disrupted, cancer_emerged, cancer_total_before_substitution, cancer_total_after_substitution,
+#     cancer_disruption_rate, cancer_emergence_rate, random_disruption_rate, random_emergence_rate,
+#     disruption_significance_uncorrected, emergence_significance_uncorrected,
+#     gene, underfitted,
+#     disruption_significance, emergence_significance,
+#     disruption_significance_uncorrected_fitting_aware, emergence_significance_uncorrected_fitting_aware = line.chomp.split("\t")
+#     RateComparisonInfo_2.new(
+#         motif,
+#         FischerTableData.new(cancer_total_before.to_i, cancer_disrupted.to_i, random_total_before.to_i, random_disrupted.to_i, 0, underfitted.to_i)
+#         FischerTableData.new(cancer_total_after.to_i,  cancer_emerged.to_i,   random_total_after.to_i,  random_emerged.to_i,   0, underfitted.to_i)
+#         official_gene_name, quality.upcase.to_sym, gene
+#     )
+#   end
+# end
+
 RateComparisonInfo = Struct.new(
                   :motif, :official_gene_name, :quality,
                   :cancer_to_random_disruption_ratio, :disruption_significance_fitting_aware,
