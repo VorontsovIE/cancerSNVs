cf8749bc4a4b213250f120f19df0c029a5713281
prijutme4ty <prijutme4ty@gmail.com>
2015-03-26 23:15:44 +0300

    refactor summary a bit; it's now underfitting aware

--- summary.rb
+++ summary.rb
@@ -21,6 +21,17 @@ def load_motif_infos(filename)
   results
 end
 
+def load_motif_underfitting_rates(fitting_log_filename)
+  return {}  unless File.exist?(fitting_log_filename)
+  File.readlines(fitting_log_filename).drop(4).slice_before{|line|
+    line.match(/^\t[^\t]/) && ! line.match(/(\d+) underfitted/)
+  }.map{|lines|
+    motif = lines[0].strip
+    underfitted = lines[1].strip.match(/^(\d+) underfitted/)[1].to_i
+    [motif, underfitted]
+  }.to_h
+end
+
 pvalue_correction_method = 'fdr'
 
 OptionParser.new do |opts|
@@ -35,9 +46,11 @@ raise 'Specify folder for cancer statistics'  unless cancer_dirname = ARGV[0] # 
 raise 'Specify folder for random statistics'  unless random_dirname = ARGV[1] # './results/motif_statistics/cpg/random'
 raise 'Specify file with motif names'  unless motif_names_filename = ARGV[2] # './source_data/motif_names.txt'
 raise 'Specify file with motif collection infos'  unless hocomoco_motifs_filename = ARGV[3] # './source_data/hocomoco_genes_infos.csv'
+raise 'Specify fitting log file'  unless fitting_log_filename = ARGV[4] # './results/motif_statistics/fitting_log/any/random_genome_13.log'
 
 motif_names = File.readlines(motif_names_filename).map(&:strip)
 motif_collection_infos = load_motif_infos(hocomoco_motifs_filename)
+fitting_logs = load_motif_underfitting_rates(fitting_log_filename)
 
 column_names =  RateComparisonInfo::COLUMN_NAMES
 
@@ -60,24 +73,53 @@ motif_infos.default_proc = ->(hsh,k) { hsh[k] = {} }
 significance_calculator = PvalueCalculator.new(class_counts: :class_and_total)
 significance_corrector = PvalueCorrector.new(pvalue_correction_method)
 
+mult = 2
 
 motif_names.each {|motif|
   motif_infos[:motif][motif] = motif
-  motif_infos[:cancer_disruption_rate][motif] = motif_infos[:cancer_disrupted][motif].to_f / motif_infos[:cancer_total_before_substitution][motif]
-  motif_infos[:cancer_emergence_rate][motif] = motif_infos[:cancer_emerged][motif].to_f / motif_infos[:cancer_total_after_substitution][motif]
 
-  motif_infos[:random_disruption_rate][motif] = motif_infos[:random_disrupted][motif].to_f / motif_infos[:random_total_before_substitution][motif]
-  motif_infos[:random_emergence_rate][motif] = motif_infos[:random_emerged][motif].to_f / motif_infos[:random_total_after_substitution][motif]
+  cancer_disrupted = motif_infos[:cancer_disrupted][motif]
+  cancer_total_before = motif_infos[:cancer_total_before_substitution][motif]
+  cancer_emerged = motif_infos[:cancer_emerged][motif]
+  cancer_total_after = motif_infos[:cancer_total_after_substitution][motif]
 
-  motif_infos[:cancer_to_random_disruption_ratio][motif] = motif_infos[:random_disruption_rate][motif] != 0 ? motif_infos[:cancer_disruption_rate][motif].to_f / motif_infos[:random_disruption_rate][motif] : nil
-  motif_infos[:cancer_to_random_emergence_ratio][motif] = motif_infos[:random_emergence_rate][motif] != 0 ? motif_infos[:cancer_emergence_rate][motif].to_f / motif_infos[:random_emergence_rate][motif] : nil
+  random_disrupted = motif_infos[:random_disrupted][motif] * mult
+  random_total_before = motif_infos[:random_total_before_substitution][motif] * mult
+  random_emerged = motif_infos[:random_emerged][motif] * mult
+  random_total_after = motif_infos[:random_total_after_substitution][motif] * mult
 
-  motif_infos[:disruption_significance_uncorrected][motif] = significance_calculator.calculate([motif_infos[:cancer_disrupted][motif], motif_infos[:random_disrupted][motif], motif_infos[:cancer_total_before_substitution][motif], motif_infos[:random_total_before_substitution][motif]])
-  motif_infos[:emergence_significance_uncorrected][motif] = significance_calculator.calculate([motif_infos[:cancer_emerged][motif], motif_infos[:random_emerged][motif], motif_infos[:cancer_total_after_substitution][motif], motif_infos[:random_total_after_substitution][motif]])
+  cancer_disruption_rate = cancer_disrupted.to_f / cancer_total_before
+  random_disruption_rate = random_disrupted.to_f / random_total_before
+
+  cancer_emergence_rate = cancer_emerged.to_f / cancer_total_after
+  random_emergence_rate = random_emerged.to_f / random_total_after
+
+  random_underfitted = (fitting_logs[motif] || 0) * mult
+
+  ###
+  motif_infos[:cancer_disruption_rate][motif] = cancer_disruption_rate
+  motif_infos[:cancer_emergence_rate][motif] = cancer_emergence_rate
+
+  motif_infos[:random_disruption_rate][motif] = random_disruption_rate
+  motif_infos[:random_emergence_rate][motif] = random_emergence_rate
+
+  motif_infos[:cancer_to_random_disruption_ratio][motif] = random_disruption_rate != 0 ? cancer_disruption_rate / random_disruption_rate : nil
+  motif_infos[:cancer_to_random_emergence_ratio][motif] = random_emergence_rate != 0 ? cancer_emergence_rate / random_emergence_rate : nil
+
+  motif_infos[:disruption_significance_uncorrected][motif] = significance_calculator.calculate([cancer_disrupted, random_disrupted, cancer_total_before, random_total_before])
+  motif_infos[:emergence_significance_uncorrected][motif] = significance_calculator.calculate([cancer_emerged, random_emerged, cancer_total_after, random_total_after])
+
+  motif_infos[:underfitted][motif] = random_underfitted
+
+  motif_infos[:disruption_significance_uncorrected_fitting_aware][motif] = significance_calculator.calculate([cancer_disrupted, random_disrupted, cancer_total_before, random_total_before], unclassified_a: 0, unclassified_b: random_underfitted)
+  motif_infos[:emergence_significance_uncorrected_fitting_aware][motif] = significance_calculator.calculate([cancer_emerged, random_emerged, cancer_total_after, random_total_after], unclassified_a: 0, unclassified_b: random_underfitted)
 }
 
 motif_infos[:disruption_significance] = significance_corrector.correct_hash(motif_infos[:disruption_significance_uncorrected])
 motif_infos[:emergence_significance] = significance_corrector.correct_hash(motif_infos[:emergence_significance_uncorrected])
+
+motif_infos[:disruption_significance_fitting_aware] = significance_corrector.correct_hash(motif_infos[:disruption_significance_uncorrected_fitting_aware])
+motif_infos[:emergence_significance_fitting_aware] = significance_corrector.correct_hash(motif_infos[:emergence_significance_uncorrected_fitting_aware])
 
 motif_infos.merge!(motif_collection_infos)
 
