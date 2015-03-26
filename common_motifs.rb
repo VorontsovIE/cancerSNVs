8bfe0513c66068f076f44812b62425017838c1bd
prijutme4ty <prijutme4ty@gmail.com>
2015-03-26 23:13:44 +0300

    fix common_motifs for a case of empty sets

--- common_motifs.rb
+++ common_motifs.rb
@@ -11,6 +11,6 @@ motif_sets = files.map do |fn|
   File.readlines(fn).drop(1).map{|l| l.chomp.split("\t").first }.to_set
 end
 
-common_motifs = motif_sets.inject(&:intersection).sort
+common_motifs = motif_sets.empty? ? [] : motif_sets.inject(&:intersection).sort
 
 print common_motifs.join("\n")
