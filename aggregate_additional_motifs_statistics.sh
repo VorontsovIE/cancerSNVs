cdbbad254f9cb20a94c39d05370af4a8b9ae64f7
prijutme4ty <prijutme4ty@gmail.com>
2015-04-08 13:12:34 +0300

    glue disruption profiles together

--- aggregate_additional_motifs_statistics.sh
+++ aggregate_additional_motifs_statistics.sh
@@ -24,3 +24,21 @@ for CONTEXT in ${CONTEXTS}; do
   ls ./results/fitted_sites/${CONTEXT}/sites_random_genome_*.txt | ruby bin/supplementary/snv_positions_in_motif_site.rb  --expand-region ${EXPAND_FLANKS_LENGTH}  --folder ./results/disruption_position_profile/${CONTEXT}/genome/
   ls ./results/fitted_sites/${CONTEXT}/sites_random_shuffle_*.txt | ruby bin/supplementary/snv_positions_in_motif_site.rb  --expand-region ${EXPAND_FLANKS_LENGTH}  --folder ./results/disruption_position_profile/${CONTEXT}/shuffle/
 done
+for CONTEXT in ${CONTEXTS}; do
+  mkdir -p ./results/disruption_position_profile/${CONTEXT}/combined
+  for MOTIF in `cat ./source_data/motif_names.txt`; do
+
+    rm ./results/disruption_position_profile/${CONTEXT}/combined/${MOTIF}.txt
+    echo -en "cancer\t" >> ./results/disruption_position_profile/${CONTEXT}/combined/${MOTIF}.txt
+    cat ./results/disruption_position_profile/${CONTEXT}/cancer/${MOTIF}.txt >> ./results/disruption_position_profile/${CONTEXT}/combined/${MOTIF}.txt
+
+    echo -en "genome\t" >> ./results/disruption_position_profile/${CONTEXT}/combined/${MOTIF}.txt
+    cat ./results/disruption_position_profile/${CONTEXT}/genome/${MOTIF}.txt >> ./results/disruption_position_profile/${CONTEXT}/combined/${MOTIF}.txt
+
+    echo -en "shuffle\t" >> ./results/disruption_position_profile/${CONTEXT}/combined/${MOTIF}.txt
+    cat ./results/disruption_position_profile/${CONTEXT}/shuffle/${MOTIF}.txt >> ./results/disruption_position_profile/${CONTEXT}/combined/${MOTIF}.txt
+
+    ruby -e 'File.write(ARGV[0], File.readlines(ARGV[0]).map{|l| l.chomp.split("\t")}.transpose.map{|l| l.join("\t") }.join("\n"))' -- ./results/disruption_position_profile/${CONTEXT}/combined/${MOTIF}.txt
+
+  done
+done
