05d659bf0fd07ca9a8c0b19d23a0d38cd12a1187
prijutme4ty <prijutme4ty@gmail.com>
2015-04-10 01:44:59 +0300

    generate_all_motif_statistics now uses less params (others are from ENV)

--- collect_motif_statistics_slices.sh
+++ collect_motif_statistics_slices.sh
@@ -3,14 +3,10 @@
 # FITTING_FOLDER and MOTIF_STATISTICS_FOLDER should be specified in environment variables
 cd "$(dirname "$0")"
 
-FOLD_CHANGE=4
-
-for CONTEXT in ${CONTEXTS}; do
-  for VARIANT  in  cancer  ${RANDOM_VARIANTS}; do
+for VARIANT  in  cancer  ${RANDOM_VARIANTS}; do
+  for CONTEXT in ${CONTEXTS}; do
     mkdir -p  ${MOTIF_STATISTICS_FOLDER}/slices/${CONTEXT}/${VARIANT}
     ./generate_all_motif_statistics.sh  ${FITTING_FOLDER}/${CONTEXT}/sites_${VARIANT}.txt  \
-                                        ${MOTIF_STATISTICS_FOLDER}/slices/${CONTEXT}/${VARIANT}  \
-                                        0.0005  $FOLD_CHANGE
-
+                                        ${MOTIF_STATISTICS_FOLDER}/slices/${CONTEXT}/${VARIANT}
   done
 done
