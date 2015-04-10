f10ebf225bc037d9b6d5394227ea8065b42347f1
prijutme4ty <prijutme4ty@gmail.com>
2015-04-10 12:18:44 +0300

    fix

--- aggregate_samplewise_paired_statistics.sh
+++ aggregate_samplewise_paired_statistics.sh
@@ -19,7 +19,7 @@ for CONTEXT in ${CONTEXTS}; do
       mkdir -p ${MOTIF_STATISTICS_FOLDER}/filtered/${SUBJECTED_OR_PROTECTED}/${CHARACTERISTIC}/${CONTEXT}/samples_vs_samples/${CANCER_TYPE_1}
       ruby filter_summary.rb  ${MOTIF_STATISTICS_FOLDER}/full/${CONTEXT}/samples_vs_samples/${CANCER_TYPE_1}/${CANCER_TYPE_2}.csv  \
                               --motif-qualities A,B,C,D  --significance 0.05  \
-                              --${CHARACTERISTIC}  --${SUBJECTED_OR_PROTECTED}  \
+                              --characteristic ${CHARACTERISTIC}  --${SUBJECTED_OR_PROTECTED}  \
                               >  ${MOTIF_STATISTICS_FOLDER}/filtered/${SUBJECTED_OR_PROTECTED}/${CHARACTERISTIC}/${CONTEXT}/samples_vs_samples/${CANCER_TYPE_1}/${CANCER_TYPE_2}.csv
     done
   done
