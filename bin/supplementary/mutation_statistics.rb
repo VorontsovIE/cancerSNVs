a4412e84c63269aa6c5131f09f2651903a053054
prijutme4ty <prijutme4ty@gmail.com>
2015-04-18 20:56:50 +0300

    extract loading cancer mutations into a separate file

--- bin/supplementary/mutation_statistics.rb
+++ bin/supplementary/mutation_statistics.rb
@@ -1,12 +1,8 @@
 # Statistics over cancer samples from Alexandrov et al.
 
 $:.unshift File.absolute_path('../../lib', __dir__)
-require 'breast_cancer_snv'
-require 'snv_info'
-require 'sample_info'
 require 'load_genome_structure'
-require 'set'
-require 'interval_notation'
+require 'cancer_mutations_loading'
 
 GENOME_FOLDER = './source_data/genome'
 EXONS_FILENAME = './source_data/exons.txt'
@@ -16,35 +12,9 @@ SOMATIC_MUTATIONS_FOLDER = File.join(ALEXANDROV_ET_AL_FOLDER, 'somatic_mutation_
 KATAEGIS_COORDINATES_FILENAME = File.join(ALEXANDROV_ET_AL_FOLDER, 'coordinates_of_kataegis.csv')
 SAMPLE_INFOS_FILENAME = File.join(ALEXANDROV_ET_AL_FOLDER, 'samples_summary.txt')
 
-def whole_genome_samples_by_cancer(sample_infos_filename)
-  samples = SampleInfo.each_in_file(sample_infos_filename).to_a
-
-  whole_genome_samples = samples.group_by(&:cancer_type).map{|cancer_type, cancer_samples|
-    [cancer_type, cancer_samples.select(&:whole_genome?).map(&:sample_name).to_set]
-  }.to_h
-end
-
-# only whole-genome, non-mitochondrial
-def load_cancer_mutations_by_cancer_type(somatic_mutations_folder, whole_genome_samples)
-  cancer_types = Dir.glob(File.join(somatic_mutations_folder, '*')).map{|dirname| File.basename(dirname).to_sym }
-
-  cancer_types.reject{|cancer_type|
-    whole_genome_samples[cancer_type].empty?
-  }.map{|cancer_type|
-    filename = File.join(somatic_mutations_folder, cancer_type.to_s, "#{cancer_type}_clean_somatic_mutations_for_signature_analysis.txt")
-    filtered_mutations = MutationInfo.each_in_file(filename).reject{|mutation|
-      mutation.chromosome == :MT
-    }.select{|mutation|
-      whole_genome_samples[cancer_type].include?(mutation.sample_id)
-    }
-
-    [cancer_type, filtered_mutations]
-  }.to_h
-end
-
 promoters_by_chromosome = load_promoters_by_chromosome(EXONS_FILENAME, length_5_prime: 5000, length_3_prime: 500, convert_chromosome_names: false)
 introns_by_chromosome = read_introns_by_chromosome(EXONS_FILENAME, convert_chromosome_names: false)
-kataegis_regions_by_chromosome = load_kataegis_regions_by_chromosome(KATAEGIS_COORDINATES_FILENAME, expanded_intervals: 1000)
+kataegis_regions_by_chromosome = load_kataegis_regions_by_chromosome(KATAEGIS_COORDINATES_FILENAME, expansion_length: 1000)
 
 whole_genome_samples = whole_genome_samples_by_cancer(SAMPLE_INFOS_FILENAME)
 mutations_by_cancer = load_cancer_mutations_by_cancer_type(SOMATIC_MUTATIONS_FOLDER, whole_genome_samples)
---
+++ lib/cancer_mutations_loading.rb
@@ -0,0 +1,29 @@
+require 'set'
+require_relative 'sample_info'
+require_relative 'snv_info'
+
+def whole_genome_samples_by_cancer(sample_infos_filename)
+  samples = SampleInfo.each_in_file(sample_infos_filename).to_a
+
+  whole_genome_samples = samples.group_by(&:cancer_type).map{|cancer_type, cancer_samples|
+    [cancer_type, cancer_samples.select(&:whole_genome?).map(&:sample_name).to_set]
+  }.to_h
+end
+
+# only whole-genome, non-mitochondrial
+def load_cancer_mutations_by_cancer_type(somatic_mutations_folder, whole_genome_samples)
+  cancer_types = Dir.glob(File.join(somatic_mutations_folder, '*')).map{|dirname| File.basename(dirname).to_sym }
+
+  cancer_types.reject{|cancer_type|
+    whole_genome_samples[cancer_type].empty?
+  }.map{|cancer_type|
+    filename = File.join(somatic_mutations_folder, cancer_type.to_s, "#{cancer_type}_clean_somatic_mutations_for_signature_analysis.txt")
+    filtered_mutations = MutationInfo.each_in_file(filename).reject{|mutation|
+      mutation.chromosome == :MT
+    }.select{|mutation|
+      whole_genome_samples[cancer_type].include?(mutation.sample_id)
+    }
+
+    [cancer_type, filtered_mutations]
+  }.to_h
+end
