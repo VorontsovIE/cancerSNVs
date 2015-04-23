b7d3d07e18a47f16986501771c8714e172e75e4a
prijutme4ty <prijutme4ty@gmail.com>
2015-04-23 09:51:46 +0300

    conversion into SNVInfo

---
+++ bin/preparations/convert_breast_cancer_snvs_to_snv_infos.rb
@@ -0,0 +1,16 @@
+$:.unshift File.absolute_path('./../../lib', __dir__)
+require 'data_import/breast_cancer_snv'
+require 'snv_info'
+require 'experiment_configuration'
+
+raise 'Specify filename'  unless filename = ARGV.first
+
+# It will output all mutations in SNVInfo format in pyrimidine context.
+# Strand indicates strand of pyrimidine context
+puts SNVInfo::HEADER
+BreastCancerSNV.each_in_file(filename).map{|snv| 
+  snv.to_snv_info(GENOME_FOLDER, flank_length: 50)
+}.map(&:in_pyrimidine_context)
+.each{|snv_info| 
+  puts snv_info
+}
