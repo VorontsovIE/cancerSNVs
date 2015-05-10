14b75658e71c2f964b475b450172c1b8c2abbabd
prijutme4ty <prijutme4ty@gmail.com>
2015-05-10 12:37:43 +0300

    remove scripts which were replaced with rake tasks

--- bin/preparations/extract_kataegis_regions.rb
+++
@@ -1,9 +0,0 @@
-# Convert Alexandrov et al. supplementary table 4 from xls into csv
-require 'roo'
-require 'roo-xls'
-
-raise 'Specify xls file from Alexandrov et al. supplementary table 4'  unless supplementary_table_filename = ARGV[0]
-xls = Roo::Excel.new(supplementary_table_filename)
-xls.drop(3).map{|line|
-  line.map{|el| el.is_a?(Numeric) ? el.to_i.to_s : el }.join("\t")
-}.each{|x| puts x}
