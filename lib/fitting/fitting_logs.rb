13622e7b7b2c6ac6833f7172fd18e504aadc6755
prijutme4ty <prijutme4ty@gmail.com>
2015-06-24 09:11:37 +0300

    fix loading fitting logs

--- lib/fitting/fitting_logs.rb
+++ lib/fitting/fitting_logs.rb
@@ -1,7 +1,7 @@
 def load_motif_underfitting_rates(fitting_log_filename)
   return {}  unless fitting_log_filename && File.exist?(fitting_log_filename)
   File.readlines(fitting_log_filename).drop(4).slice_before{|line|
-    line.match(/^\t[^\t]/) && ! line.match(/(\d+) underfitted/)
+    line.match(/^\t[^\t]/) && ! line.match(/(\d+) underfitted/) && ! line.match(/found \d+ from \d+/)
   }.map{|lines|
     motif = lines[0].strip
     underfitted = lines[1].strip.match(/^(\d+) underfitted/)[1].to_i
